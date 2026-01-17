a2e14b4216b11f342e6747987e60ea6385a30339
Mikhail Berdnikov <berdnikovmiha@gmail.com>
2026-01-17 13:05:19 +0300

    Initial

---
+++ app/src/main/java/com/Goldy/blindchess/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.Goldy.blindchess
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.Goldy.blindchess.ui.theme.BlindChessTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // Вычисляем класс размера окна
+            val windowSizeClass = calculateWindowSizeClass(this)
+
+            BlindChessTheme {
+                // Передаем этот класс в ваш главный Composable
+                MainScreen(windowSizeClass)
+            }
+        }
+    }
+}
+
